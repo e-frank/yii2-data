23da9d1f7fc51f755bead7488049f52658549bba
Elmar Frank <elmar.frank@efrank.at>
2014-12-15 15:14:13 +0100

    minor

--- ActiveDocument.php
+++ ActiveDocument.php
@@ -37,8 +37,15 @@ class ActiveDocument extends yii\base\Model
 {
     const ACTIVE_DOCUMENT = 'ActiveDocument';
 
-    public $config = [];
-    public $model  = [];
+    public $config             = [];
+    public $useTransaction     = true;
+    public $defaultIncremental = false;
+    public $defaultIgnoreError = false;
+    public $defaultDelete      = false;
+    public $defaultScenario    = null;
+    public $defaultSkipUpdate  = false;
+
+    public $model              = null;
 
 
 
@@ -47,7 +54,7 @@ class ActiveDocument extends yii\base\Model
     }
 
 
-    public static function find($model, $options, $key) {
+    public static function find222($model, $options, $key) {
         if (is_string($model))
             $model = new $model();
 
@@ -57,6 +64,35 @@ class ActiveDocument extends yii\base\Model
             self::attach($model, $options);
         }
         return $model;
+    }
+
+    private function getConfig() {
+        return [
+            'useTransaction'     => $this->useTransaction,
+            'defaultIncremental' => $this->defaultIncremental,
+            'defaultIgnoreError' => $this->defaultIgnoreError,
+            'defaultDelete'      => $this->defaultDelete,
+            'defaultScenario'    => $this->defaultScenario,
+            'defaultSkipUpdate'  => $this->defaultSkipUpdate,
+            'config'             => $this->config,
+        ];
+    }
+
+    public function init() {
+        if (is_string($this->model))
+            $this->model = new $this->model();
+        self::attach($this->model, $this->getConfig());
+    }
+
+    public function findOne($key) {
+        $result = $this->model->findWithRelations($key);
+        if ($result)
+            self::attach($result, $this->getConfig());
+        return $result;
+    }
+
+    public function load($data, $formName = null) {
+        return $this->model->load($data, $formName);
     }
 
 }
